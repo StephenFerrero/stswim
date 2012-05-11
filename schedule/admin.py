50dcbe5f54162390110dc347a538818aedf18bf9
unknown <sferrero@XAN-OAK-L040.xantrion-hq.local>
2012-05-11 09:16:23 -0700

    CSV Export for Parents in admin interface

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from actions import export_as_csv_action
 from stswim.schedule.models import Household, Parent, Student, Employee, Season, Lesson, LessonSlot
 
-for model in [Household, Parent, Student, Employee, Season, Lesson, LessonSlot,]:
+class ParentAdmin(admin.ModelAdmin)
+	list_display = ('first_name', 'last_name','email','phone_number',)
+	actions = [export_as_csv_action("CSV Export", fields=['first_name', 'last_name','email','phone_number','address','address2','city','state','zip_code',])]
+
+
+for model in [Household, Student, Employee, Season, Lesson, LessonSlot,]:
     admin.site.register(model)
