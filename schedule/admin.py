fa7e3d4984189d2649063b5589c0162c579375fb
unknown <sferrero@XAN-OAK-L040.xantrion-hq.local>
2012-05-11 09:23:16 -0700

    syntax error

--- schedule/admin.py
+++ schedule/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from actions import export_as_csv_action
 from stswim.schedule.models import Household, Parent, Student, Employee, Season, Lesson, LessonSlot
 
-class ParentAdmin(admin.ModelAdmin)
+class ParentAdmin(admin.ModelAdmin):
 	list_display = ('first_name', 'last_name','email','phone_number',)
 	actions = [export_as_csv_action("CSV Export", fields=['first_name', 'last_name','email','phone_number','address','address2','city','state','zip_code',])]
 admin.site.register(Parent, ParentAdmin)
